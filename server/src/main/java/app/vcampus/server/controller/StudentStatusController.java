f5c5644f2bfe49d9880f1797003470a7d7fdc315
DKirito <1821150594@qq.com>
2023-08-27 19:12:15 +0800

    feat(StudentStatusController):delete and search student’s information

--- server/src/main/java/app/vcampus/server/controller/StudentStatusController.java
+++ server/src/main/java/app/vcampus/server/controller/StudentStatusController.java
@@ -18,6 +18,7 @@ public class StudentStatusController {
     Solve client to update student status information and update to the database
     The constraints are cardNumber  != null and when no such student throw exception
     Test passed on 2023/08/27
+    Test : {"uri":"student/updateInfo","params":{"cardNumber":"1000","studentNumber":"15","major":"2","school":"2"}}
     */
     @RouteMapping(uri = "student/updateInfo")
     public Response updateInfo(Request request, org.hibernate.Session database) {
@@ -53,6 +54,7 @@ public class StudentStatusController {
     Solve client to add student status information and update to the database
     The constraints are cardNumber and studentNumber != null
     Test passed on 2023/08/27
+    Test : {"uri":"student/addInfo","params":{"cardNumber":"999","studentNumber":"15","birthPlace":"SEU"}}
      */
     @RouteMapping(uri = "student/addInfo")
     public Response addInfo(Request request, org.hibernate.Session database){
@@ -79,5 +81,58 @@ public class StudentStatusController {
         tx.commit();
         return Response.Common.ok();
     }
-}
 
+    /*
+    Solve client to delete student status information and update to the database
+    The constraints : cardNumber  != null  && database have the student's cardNumber
+    Test passed on 2023/08/27
+    */
+    @RouteMapping(uri = "student/deleteInfo")
+    public Response deleteInfo(Request request, org.hibernate.Session database){
+        String cardNumber = request.getParams().get("cardNumber");
+
+        if (cardNumber == null) {
+            return Response.Common.error("card number cannot be empty");
+        }
+
+        Student student = database.get(Student.class,Integer.parseInt(cardNumber));
+
+        if(student == null){
+            return Response.Common.error("no such card number");
+        }
+
+        Transaction tx = database.beginTransaction();
+        database.delete(student);
+        tx.commit();
+        return Response.Common.ok();
+    }
+
+
+    /*
+    Solve client to search student status information and update to the database
+    The constraints : cardNumber  != null  && database have the student's cardNumber
+    Test passed on 2023/08/27
+    Test:{"uri":"student/searchInfo","params":{"cardNumber":"250"}}
+    */
+    @RouteMapping(uri = "student/searchInfo")
+    public Response searchInfo(Request request, org.hibernate.Session database){
+        String cardNumber = request.getParams().get("cardNumber");
+
+        if (cardNumber == null) {
+            return Response.Common.error("card number cannot be empty");
+        }
+
+        Student student = database.get(Student.class,Integer.parseInt(cardNumber));
+
+        if(student == null){
+            return Response.Common.error("no such card number");
+        }
+
+        System.out.println(student.getCardNumber());
+        System.out.println(student.getStudentNumber());
+        System.out.println(student.getBirthPlace());
+        System.out.println(student.getSchool());
+        System.out.println(student.getMajor());
+        return Response.Common.ok();
+    }
+}
