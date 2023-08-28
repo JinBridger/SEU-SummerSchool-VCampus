34e64fbad73f4d4044817109fe11ee3455929f68
GreatSlove <505909828@qq.com>
2023-08-28 16:21:00 +0800

    Course class

---
+++ server/src/main/java/app/vcampus/server/entity/Course.java
@@ -0,0 +1,10 @@
+package app.vcampus.server.entity;
+
+public class Course {
+    public Integer courseID;
+    public String courseName;
+
+    public String school;//School offered
+    public float credit;//credit
+
+}
