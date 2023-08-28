d1ca9d70cfe08bf5b844369166cd18f21a30b74c
GreatSlove <505909828@qq.com>
2023-08-28 16:32:45 +0800

    refactor(server):course.java

--- server/src/main/java/app/vcampus/server/entity/Course.java
+++ server/src/main/java/app/vcampus/server/entity/Course.java
@@ -1,9 +1,23 @@
 package app.vcampus.server.entity;
 
-public class Course {
-    public Integer courseID;
-    public String courseName;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Table(name = "course")
+@Slf4j
+public class Course {
+    @Id
+    @Column(name = "courseId")
+    public Integer courseID;
+    @Column(nullable = false)
+    public String courseName;
+    @Column(nullable = false)
     public String school;//School offered
     public float credit;//credit
 
