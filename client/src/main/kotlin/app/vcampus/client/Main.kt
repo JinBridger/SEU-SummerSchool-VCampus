c9f930fbb8297e6d38805c8c60eb0826e92a834c
jinbridge <2635480475@qq.com>
2023-08-30 16:38:03 +0800

    feat(client): new titlebar

--- client/src/main/kotlin/app/vcampus/client/Main.kt
+++ client/src/main/kotlin/app/vcampus/client/Main.kt
@@ -1,24 +1,37 @@
 package app.vcampus.client
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.window.WindowDraggableArea
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import app.vcampus.client.net.NettyHandler
 import app.vcampus.client.repository.FakeRepository
+import app.vcampus.client.scene.components.TopBar
 import moe.tlaster.precompose.PreComposeWindow
 
 fun main(handler: NettyHandler) {
     FakeRepository.setHandler(handler)
     application {
+    val state = rememberWindowState(size = DpSize(1400.dp, 800.dp))
         PreComposeWindow(
-            title = "PreCompose Sample",
-            onCloseRequest = {
-                exitApplication()
-            },
-            state = WindowState(size = DpSize(1400.dp, 800.dp))
+                title = "PreCompose Sample",
+                onCloseRequest = {
+                    exitApplication()
+                },
+                state = state,
+                undecorated = true,
         ) {
-            App()
+            Column {
+                WindowDraggableArea {
+                    TopBar("VCampus", { exitApplication() })
+                }
+                App()
+            }
         }
     }
 }
--- client/src/main/kotlin/app/vcampus/client/scene/components/TopBar.kt
+++ client/src/main/kotlin/app/vcampus/client/scene/components/TopBar.kt
@@ -5,35 +5,32 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
 
 @Composable
-fun TopBar(heading: String) {
+fun TopBar(heading: String, onClose: () -> Unit) {
     TopAppBar(
-        title = {
-            Text(
-                text = heading,
-                style = TextStyle(
-                    fontSize = 20.sp,
-                    lineHeight = 24.sp,
-                    fontWeight = FontWeight(600),
-                    color = Color(0xFFFFFFFF),
-                    letterSpacing = 0.15.sp,
+            title = {
+                Text(
+                        text = heading,
+                        style = TextStyle(
+                                fontSize = 20.sp,
+                                lineHeight = 24.sp,
+                                fontWeight = FontWeight(600),
+                                color = Color(0xFFFFFFFF),
+                                letterSpacing = 0.15.sp,
+                        )
                 )
-            )
-        },
-        actions = {
-            IconButton(onClick = {}) {
-                Icon(Icons.Default.Notifications, contentDescription = null)
-            }
-        },
+            },
+            actions = {
+                IconButton(onClick = { onClose() }) {
+                    Icon(Icons.Default.Close, contentDescription = null)
+                }
+            },
     )
 }
