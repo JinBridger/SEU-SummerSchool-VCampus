9ae2c9169ae023e04fcae40eb519aed9fb0d0762
GreatSlove <505909828@qq.com>
2023-08-30 20:04:06 +0800

    feat(server):addCourse selectClass

--- server/src/main/java/app/vcampus/server/controller/TeachingAffairsController.java
+++ server/src/main/java/app/vcampus/server/controller/TeachingAffairsController.java
@@ -1,56 +1,55 @@
 package app.vcampus.server.controller;
 
-
+import app.vcampus.server.entity.Course;
+import app.vcampus.server.entity.SelectedClass;
+import app.vcampus.server.entity.Student;
+import app.vcampus.server.entity.User;
+import app.vcampus.server.utility.Database;
+import app.vcampus.server.utility.Request;
+import app.vcampus.server.utility.Response;
+import app.vcampus.server.utility.router.RouteMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Transaction;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TeachingAffairsController {
-//    @RouteMapping(uri = "course/addCourse")
-//    public Response addCourse(Request request, org.hibernate.Session database) {
-//        Course newCourse = Course.fromMap(request.getParams());
-//        if (newCourse == null) {
-//            return Response.Common.badRequest();
-//        }
-//
-//        Course user = database.get(Course.class, newCourse.getCourseId());
-//        if (user == null) {
-//            return Response.Common.error("Course not found");
-//        }
-//
-//        Transaction tx = database.beginTransaction();
-//        database.persist(newCourse);
-//        tx.commit();
-//
-//        return Response.Common.ok();
-//    }
-//
-////    @RouteMapping(uri="class/searchClass")
-////    public Response searchClass(Request request,org.hibernate.Session database)
-////    {
-////        String courseId=request.getParams().get("courseId");
-////        if(courseId==null){
-////            return Response.Common.error("course id could not be empty");
-////        }
-////
-////        Class class
-////    }
-//
-//    @RouteMapping(uri = "class/addClass")
-//    public  Response addClass(Request request,org.hibernate.Session database)
-//    {
-//        TeachingClass newclass= TeachingClass.fromMap(request.getParams());
-//        if(newclass==null)return Response.Common.badRequest();
-//
-//        User user=database.get(User.class,newclass.getCourseId());
-//        if(user==null){
-//            return Response.Common.error("not found");
-//        }
-//
-//        Transaction tx=database.beginTransaction();
-//        database.persist(newclass);
-//        tx.commit();
-//
-//        return Response.Common.ok();
-//    }
+    @RouteMapping(uri = "course/addCourse")
+    public Response addCourse(Request request, org.hibernate.Session database) {
+        Course newCourse = Course.fromMap(request.getParams());
+        if (newCourse == null) {
+            return Response.Common.badRequest();
+        }
+
+        Course user = database.get(Course.class, newCourse.getCourseId());
+        if (user == null) {
+            return Response.Common.error("Course not found");
+        }
+
+        Transaction tx = database.beginTransaction();
+        database.persist(newCourse);
+        tx.commit();
+
+        return Response.Common.ok();
+    }
+
+    /*This method is used to select courses for students*/
+    @RouteMapping(uri="selectedClass/selectClass")
+    public Response selectclass(Request request, org.hibernate.Session database) {
+        SelectedClass newSelectedClass = SelectedClass.fromMap(request.getParams());
+        if (newSelectedClass == null) {
+            return Response.Common.badRequest();
+        }
+        SelectedClass selectedClass = database.get(SelectedClass.class, newSelectedClass.getCardNumber());
+        if (selectedClass == null) {
+            return Response.Common.error("SelectedClass not found");
+        }
+
+        Transaction tx = database.beginTransaction();
+        database.persist(newSelectedClass);
+        tx.commit();
+        return Response.Common.ok();
+    }
+
 
 }
