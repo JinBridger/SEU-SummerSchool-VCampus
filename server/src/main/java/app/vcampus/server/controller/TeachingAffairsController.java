5a718bbff87bf006d706f3a6d56febe40f13a3b2
GreatSlove <505909828@qq.com>
2023-09-01 09:23:56 +0800

    feat(server): add searchInfo method

--- server/src/main/java/app/vcampus/server/controller/TeachingAffairsController.java
+++ server/src/main/java/app/vcampus/server/controller/TeachingAffairsController.java
@@ -1,9 +1,6 @@
 package app.vcampus.server.controller;
 
-import app.vcampus.server.entity.Course;
-import app.vcampus.server.entity.SelectedClass;
-import app.vcampus.server.entity.Student;
-import app.vcampus.server.entity.User;
+import app.vcampus.server.entity.*;
 import app.vcampus.server.utility.Database;
 import app.vcampus.server.utility.Request;
 import app.vcampus.server.utility.Response;
@@ -49,6 +46,43 @@ public class TeachingAffairsController {
         database.persist(newSelectedClass);
         tx.commit();
         return Response.Common.ok();
+    }
+
+    /*for student to search selectedClass information*/
+    @RouteMapping(uri="selectedClass/searchInfo")
+    public Response searchInfo(Request request,org.hibernate.Session database){
+        String cardNumber=request.getParams().get("cardNumber");
+
+        if(cardNumber==null){
+            return Response.Common.error("card number cannot be empty");
+        }
+
+        SelectedClass selectedClass=database.get(SelectedClass.class,Integer.parseInt(cardNumber));
+
+        if(selectedClass==null){
+            return Response.Common.error("no such card number");
+        }
+        System.out.println(selectedClass);
+        return Response.Common.ok(selectedClass.toMap());
+    }
+
+    /*for teacher to search teaching class information*/
+    @RouteMapping(uri="class/searchClass")
+    public Response searchClass(Request request,org.hibernate.Session database){
+        String cardNumber=request.getParams().get("cardNumber");
+
+        if(cardNumber==null){
+            return Response.Common.error("card number cannot be empty");
+        }
+
+        TeachingClass teachingClass=database.get(TeachingClass.class,Integer.parseInt(cardNumber));
+        if(teachingClass==null){
+            return Response.Common.error("no such card number");
+        }
+
+        System.out.println(teachingClass);
+
+        return Response.Common.ok(teachingClass.toMap());
     }
 
 
