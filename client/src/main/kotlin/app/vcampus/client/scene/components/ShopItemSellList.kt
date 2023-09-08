fc4a73ab1a6607c1683a44965a5c1283214fdd94
jinbridge <2635480475@qq.com>
2023-09-08 08:49:44 +0800

    feat(client): store dashboard ui

---
+++ client/src/main/kotlin/app/vcampus/client/scene/components/ShopItemSellList.kt
@@ -0,0 +1,84 @@
+package app.vcampus.client.scene.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import app.vcampus.client.viewmodel.ShopViewModel
+import app.vcampus.server.entity.StoreItem
+
+@Composable
+fun shopItemSellListItem(item: StoreItem, viewModel: ShopViewModel) {
+    Surface(modifier = Modifier.fillMaxWidth().border(
+            1.dp,
+            color = Color.LightGray,
+            shape = RoundedCornerShape(4.dp)
+    )) {
+        Column {
+            Row(modifier = Modifier.fillMaxWidth().height(150.dp).padding(
+                    6.dp)) {
+                Box(modifier = Modifier.aspectRatio(1F).fillMaxHeight().clip(
+                        RoundedCornerShape(4.dp))) {
+                    AsyncImage(
+                            load = { loadImageBitmap(item.pictureLink) },
+                            painterFor = { remember { BitmapPainter(it) } },
+                            contentDescription = "",
+                            contentScale = ContentScale.FillBounds)
+//                    Image(painterResource("test_image.png"), "",
+//                            modifier = Modifier.fillMaxSize(),
+//                            contentScale = ContentScale.FillBounds)
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = item.itemName,
+                            fontWeight = FontWeight(700), maxLines = 1,
+                            overflow = TextOverflow.Ellipsis)
+                    Spacer(modifier = Modifier.weight(1F))
+                    Row(modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End,
+                            verticalAlignment = Alignment.CenterVertically) {
+                        Spacer(modifier = Modifier.width(6.dp))
+                        Column {
+                            Row {
+                                Text("销量：", fontWeight = FontWeight(700))
+                                Text(item.salesVolume.toString())
+                            }
+                            Row {
+                                Text("库存：", fontWeight = FontWeight(700))
+                                Text(item.stock.toString())
+                            }
+                        }
+                        Spacer(modifier = Modifier.width(6.dp))
+                    }
+                    Spacer(modifier = Modifier.width(6.dp))
+
+                    Spacer(modifier = Modifier.weight(1F))
+                    Row(modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.Bottom) {
+                        Column {
+                            Text("￥", fontSize = 14.sp)
+                            Spacer(modifier = Modifier.height(10.dp))
+                        }
+                        Text(String.format("%.2f", item.price / 100.0),
+                                fontWeight = FontWeight(700),
+                                fontSize = 24.sp)
+                        Spacer(modifier = Modifier.weight(1F))
+                    }
+                }
+            }
+        }
+    }
+}
--- client/src/main/kotlin/app/vcampus/client/scene/subscene/shop/DashboardSubscene.kt
+++ client/src/main/kotlin/app/vcampus/client/scene/subscene/shop/DashboardSubscene.kt
@@ -1,15 +1,32 @@
 package app.vcampus.client.scene.subscene.shop
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.vcampus.client.scene.components.pageTitle
+import app.vcampus.client.scene.components.shopItemSellListItem
 import app.vcampus.client.viewmodel.ShopViewModel
+import app.vcampus.server.entity.StoreItem
 
 @Composable
 fun dashboardSubscene(viewModel: ShopViewModel) {
+    val testItem = StoreItem()
+    testItem.itemName = "测试名称"
+    testItem.pictureLink = "https://i.dawnlab.me/05f0f5392e8efc95de553bafa2e30722.png"
+    testItem.price = 279900
+    testItem.salesVolume = 100
+    testItem.stock = 100
+
     Box(modifier = Modifier.fillMaxSize().padding(horizontal = 100.dp)) {
         Row(horizontalArrangement = Arrangement.Center,
                 modifier = Modifier.fillMaxWidth()) {
@@ -17,6 +34,40 @@ fun dashboardSubscene(viewModel: ShopViewModel) {
                 item {
                     Spacer(Modifier.height(80.dp))
                     pageTitle("查看后台信息", "仪表盘")
+                    Spacer(Modifier.height(20.dp))
+                }
+
+                item {
+                    Row(Modifier.fillMaxWidth()) {
+                        Text("今日销售额", fontWeight = FontWeight(700),
+                                fontSize = 20.sp)
+                        Spacer(Modifier.weight(1F))
+                        Column {
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text("￥", fontSize = 14.sp)
+                        }
+                        Text(String.format("%.2f", 9999999 / 100.0),
+                                fontWeight = FontWeight(700),
+                                fontSize = 24.sp)
+                    }
+                }
+
+                item {
+                    Spacer(Modifier.height(20.dp))
+                    Text("商品销售情况", fontWeight = FontWeight(700),
+                            fontSize = 20.sp)
+                    Spacer(Modifier.height(10.dp))
+                }
+
+                (0..10).forEach {
+                    item {
+                        shopItemSellListItem(testItem, viewModel)
+                        Spacer(Modifier.height(10.dp))
+                    }
+                }
+
+                item {
+                    Spacer(Modifier.height(80.dp))
                 }
             }
         }
