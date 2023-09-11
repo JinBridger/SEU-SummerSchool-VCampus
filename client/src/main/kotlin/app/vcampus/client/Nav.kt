4ebd06c0cfb474bf7a8d2f20c4976d6cdc147c1c
jinbridge <2635480475@qq.com>
2023-09-11 22:58:09 +0800

    feat(client): permission on navrail

--- client/src/main/kotlin/app/vcampus/client/Nav.kt
+++ client/src/main/kotlin/app/vcampus/client/Nav.kt
@@ -7,16 +7,17 @@ import androidx.compose.ui.graphics.vector.ImageVector
 data class NaviItem(
         val name: String,
         val path: String,
-        val icon: ImageVector
+        val icon: ImageVector,
+        val permission: List<String>
 )
 
 val Navis = arrayListOf(
-        NaviItem("主页", "/home", Icons.Default.Home),
-        NaviItem("学籍", "/student_status", Icons.Default.Person),
-        NaviItem("教务", "/teaching_affairs", Icons.Default.School),
-        NaviItem("图书馆", "/library", Icons.Default.Book),
-        NaviItem("超市", "/shop", Icons.Default.LocalMall),
-        NaviItem("财务", "/finance", Icons.Default.Savings),
-        NaviItem("管理员", "/admin", Icons.Default.ManageAccounts),
-        NaviItem("GPT", "/GPT", Icons.Default.Assistant)
+        NaviItem("主页", "/home", Icons.Default.Home, listOf("user")),
+        NaviItem("学籍", "/student_status", Icons.Default.Person, listOf("student", "affairs_staff")),
+        NaviItem("教务", "/teaching_affairs", Icons.Default.School, listOf("student", "teacher")),
+        NaviItem("图书馆", "/library", Icons.Default.Book, listOf("library_user", "library_staff")),
+        NaviItem("超市", "/shop", Icons.Default.LocalMall, listOf("shop_user", "shop_staff")),
+        NaviItem("财务", "/finance", Icons.Default.Savings, listOf("finance_user", "finance_staff")),
+        NaviItem("管理员", "/admin", Icons.Default.ManageAccounts, listOf("admin")),
+        NaviItem("GPT", "/GPT", Icons.Default.Assistant, listOf("user"))
 )
--- client/src/main/kotlin/app/vcampus/client/scene/components/NavRail.kt
+++ client/src/main/kotlin/app/vcampus/client/scene/components/NavRail.kt
@@ -6,21 +6,25 @@ import androidx.compose.material.NavigationRailItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import app.vcampus.client.Navis
+import app.vcampus.client.repository.FakeRepository
 import moe.tlaster.precompose.navigation.Navigator
 
 @Composable
 fun NavRail(navigator: Navigator, currentPos: String) {
     NavigationRail {
         Navis.forEach {
-            NavigationRailItem(
-                    selected = currentPos == it.path,
-                    onClick = {
-                        if (currentPos != it.path) navigator.navigate(it.path)
-                    },
-                    icon = { Icon(it.icon, "") },
-                    label = { Text(it.name) },
-                    alwaysShowLabel = false
-            )
+            if (it.permission.intersect(
+                            FakeRepository.user.roles.toList()).isNotEmpty())
+                NavigationRailItem(
+                        selected = currentPos == it.path,
+                        onClick = {
+                            if (currentPos != it.path) navigator.navigate(
+                                    it.path)
+                        },
+                        icon = { Icon(it.icon, "") },
+                        label = { Text(it.name) },
+                        alwaysShowLabel = false
+                )
         }
     }
 }
